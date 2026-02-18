3189978252e81b4f8068d0f97a423d4ca71446e6
azanfraz7 <azanfraz11@gmail.com>
2026-02-18 17:53:55 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -35,7 +35,7 @@ Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
 */
 float fahrenheitToCelsius(float fahrenheit) {
-    float celsius = (fahrenheit-32) * 5/9;
+    float celsius = (fahrenheit-32)*5/9;
     return celsius;
     //return celsius;
 
